81d2f1d0334707b80aea4cd939faeeddc017e4e5
tanvigunjal <tanvigunjal97@gmail.com>
2025-11-07 20:52:45 +0100

    updating inference_profilder.sh

--- inference_profiler.sh
+++ inference_profiler.sh
@@ -6,16 +6,15 @@ export MKL_NUM_THREADS=1
 # - `--num-runs`: Controls the number of benchmark iterations for stable, averaged metrics.
 # - `--profile`: Enables cProfile and torch.profiler for a detailed trace of the first run.
 # - `--profile_memory`: Enables memory tracking within the torch.profiler.
-python -m scripts.inference_profiler \
+python -m scripts.inference_profiler_optimized \
     --unet_config_path "configs/unet/stage2_512.yaml" \
     --inference_ckpt_path "checkpoints/latentsync_unet.pt" \
     --inference_steps 10 \
     --guidance_scale 2 \
+    --enable_deepcache \
     --video_path "assets/demo_video.mp4" \
     --audio_path "assets/demo_audio.wav" \
     --video_out_path "video_out.mp4" \
-    --num_profile_runs 2 \
-    --profile \
-    --profile_memory
-
-#     --enable_deepcache \
+    # --num_profile_runs 2 \
+    # --profile \
+    # --profile_memory
